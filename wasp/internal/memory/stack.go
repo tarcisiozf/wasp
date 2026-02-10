1cbb2d4ac82bca3cb00302934cb35a54983c4bde
Tarcisio Ferraz <tarcisio.ferraz@smartcontract.com>
2026-02-10 09:47:12 -0300

    fix stack

--- wasp/internal/memory/stack.go
+++ wasp/internal/memory/stack.go
@@ -13,20 +13,22 @@ func (s *Stack) Push(value any) {
 }
 
 func (s *Stack) Pop() any {
-	if len(s.items) == 0 {
+	size := len(s.items)
+	if size == 0 {
 		panic("stack underflow")
 	}
 
-	value := s.items[len(s.items)-1]
-	s.items = s.items[:len(s.items)-1]
+	value := s.items[size-1]
+	s.items = s.items[:size-1]
 	return value
 }
 
 func (s *Stack) PopN(n int) []any {
-	if n > len(s.items) {
+	size := len(s.items)
+	if n > size {
 		panic("stack underflow")
 	}
-	items := s.items[:n]
-	s.items = s.items[n:]
+	items := s.items[size-n:]
+	s.items = s.items[:size-n]
 	return items
 }
