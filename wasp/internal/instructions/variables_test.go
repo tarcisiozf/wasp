6c851743668e1b352b36558f44af62c8a2b1643d
Tarcisio Ferraz <tarcisio.ferraz@smartcontract.com>
2026-02-14 11:02:31 -0300

    test globals

--- wasp/internal/instructions/variables_test.go
+++ wasp/internal/instructions/variables_test.go
@@ -37,3 +37,33 @@ func TestLocals(t *testing.T) {
 	logSpy.CalledOnce(t)
 	logSpy.FirstCall().CalledWith(t, 10)
 }
+
+func TestGlobals(t *testing.T) {
+	linker := wasp.NewLinker()
+	logSpy := tests.NewSpy()
+	assert.NoError(t, linker.Define("console", "log", logSpy.Called))
+
+	testEnv := tests.NewEnvironment(
+		tests.WithInstanceOptions(
+			wasp.WithLinker(linker),
+		),
+	)
+	_, _, err := testEnv.RunWat(t, `
+		(module
+		  (import "console" "log" (func $log (param i32)))
+		  (global $var (mut i32) (i32.const 0))
+		  (func $main
+			i32.const 10 ;; load a number onto the stack
+			global.set $var ;; set the $var
+		
+			global.get $var ;; load $var onto the stack
+			call $log ;; log the result
+		  )
+		  (start $main)
+		)
+	`)
+	assert.NoError(t, err)
+
+	logSpy.CalledOnce(t)
+	logSpy.FirstCall().CalledWith(t, 10)
+}
