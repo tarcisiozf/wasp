831a788a4637586cdedfd3a4ad0731db6c728e7d
Tarcisio Ferraz <tarcisio.ferraz@smartcontract.com>
2026-02-15 15:19:37 -0300

    extract

--- wasp/debug/debug.go
+++ wasp/debug/debug.go
@@ -1,4 +1,4 @@
-package module
+package debug
 
 import (
 	"fmt"
@@ -8,80 +8,100 @@ import (
 	"wasp/wasp/internal/opcodes"
 )
 
-func Foo(data []byte, index, offset int) {
+func FuncToString(data []byte, index, offset int) (str string, err error) {
 	iter := binary.NewIterator(data)
-	fmt.Printf("; function body %d\n", index)
-	f(offset, "func body size", len(data))
+	str = fmt.Sprintf("; function body %d\n", index)
+	str += f(offset, "func body size", len(data))
 
 	localDeclCount := iter.Varint()
-	f(offset+iter.Position(), "local decl count", localDeclCount)
+	str += f(offset+iter.Position(), "local decl count", localDeclCount)
 
 	for i := 0; i < localDeclCount; i++ {
 		localTypeCount := iter.Varint()
-		f(offset+iter.Position(), "local type count", localTypeCount)
+		str += f(offset+iter.Position(), "local type count", localTypeCount)
 
 		localType := iter.Byte()
-		f(offset+iter.Position(), typeToString(localType), localType)
+		str += f(offset+iter.Position(), typeToString(localType), localType)
 	}
 
 	for iter.HasNext() {
 		opcode := iter.Opcode()
-		f(offset+iter.Position(), opcodeName(opcode), opcode)
-		k(iter, offset, opcode)
+		str += f(offset+iter.Position(), opcodeName(opcode), opcode)
+		lines, err := k(iter, offset, opcode)
+		if err != nil {
+			return str, err
+		}
+		str += lines
 	}
+	return str, nil
 }
 
 func opcodeName(opcode opcodes.Opcode) string {
 	return opcodes.Name(opcode)
 }
 
-func k(iter *binary.Iterator, offset int, opcode opcodes.Opcode) {
+func k(iter *binary.Iterator, offset int, opcode opcodes.Opcode) (str string, err error) {
 	switch opcode {
 	case opcodes.GlobalGet, opcodes.GlobalSet:
-		f(offset+iter.Position(), "global index", iter.Varint())
+		return f(offset+iter.Position(), "global index", iter.Varint()), nil
+
 	case opcodes.I32Const:
-		f(offset+iter.Position(), "i32 literal", iter.Varint())
+		return f(offset+iter.Position(), "i32 literal", iter.Varint()), nil
+
 	case opcodes.I64Const:
-		f(offset+iter.Position(), "i64 literal", iter.Varint())
+		return f(offset+iter.Position(), "i64 literal", iter.Varint()), nil
+
 	case opcodes.F64Const:
-		f(offset+iter.Position(), "f64 literal", iter.Float64())
+		return f(offset+iter.Position(), "f64 literal", iter.Float64()), nil
+
 	case opcodes.F32Const:
-		f(offset+iter.Position(), "f32 literal", iter.Float32())
+		return f(offset+iter.Position(), "f32 literal", iter.Float32()), nil
+
 	case opcodes.LocalTee, opcodes.LocalGet, opcodes.LocalSet:
-		f(offset+iter.Position(), "local index", iter.Varint())
+		return f(offset+iter.Position(), "local index", iter.Varint()), nil
+
 	case opcodes.Block, opcodes.Loop:
-		f(offset+iter.Position(), typeToString(iter.Peek()), iter.Byte())
+		return f(offset+iter.Position(), typeToString(iter.Peek()), iter.Byte()), nil
+
 	case opcodes.Br, opcodes.BrIf:
-		f(offset+iter.Position(), "break depth", iter.Varint())
+		return f(offset+iter.Position(), "break depth", iter.Varint()), nil
+
 	case opcodes.I32Load8U, opcodes.I32Load8S, opcodes.I32Load,
 		opcodes.I64Load, opcodes.I32Load16S, opcodes.I32Load16U,
 		opcodes.I64Load8S, opcodes.I64Load8U, opcodes.I64Load16S,
 		opcodes.I64Load16U, opcodes.I64Load32S, opcodes.I64Load32U,
 		opcodes.F64Load, opcodes.F32Load:
-		f(offset+iter.Position(), "alignment", iter.Byte())
-		f(offset+iter.Position(), "load offset", iter.Varint())
+		return f(offset+iter.Position(), "alignment", iter.Byte()) +
+			f(offset+iter.Position(), "load offset", iter.Varint()), nil
+
 	case opcodes.I32Store, opcodes.I64Store, opcodes.I32Store16,
 		opcodes.I32Store8, opcodes.I64Store16, opcodes.I64Store8,
 		opcodes.I64Store32, opcodes.F64Store, opcodes.F32Store:
-		f(offset+iter.Position(), "alignment", iter.Byte())
-		f(offset+iter.Position(), "store offset", iter.Varint())
+		return f(offset+iter.Position(), "alignment", iter.Byte()) +
+			f(offset+iter.Position(), "store offset", iter.Varint()), nil
+
 	case opcodes.Call, opcodes.ReturnCall:
-		f(offset+iter.Position(), "function index", iter.Varint())
+		return f(offset+iter.Position(), "function index", iter.Varint()), nil
+
 	case opcodes.CallIndirect:
-		f(offset+iter.Position(), "signature index", iter.Varint())
-		f(offset+iter.Position(), "table index", iter.Varint())
+		return f(offset+iter.Position(), "signature index", iter.Varint()) +
+			f(offset+iter.Position(), "table index", iter.Varint()), nil
+
 	case opcodes.BrTable:
 		numTargets := iter.Varint()
-		f(offset+iter.Position(), "num targets", numTargets)
+		str = f(offset+iter.Position(), "num targets", numTargets)
 		for i := 0; i < numTargets; i++ {
-			f(offset+iter.Position(), "break depth", iter.Varint())
+			str += f(offset+iter.Position(), "break depth", iter.Varint())
 		}
-		f(offset+iter.Position(), "break depth for default", iter.Varint())
+		return str + f(offset+iter.Position(), "break depth for default", iter.Varint()), nil
+
 	case opcodes.MemoryFill, opcodes.MemorySize, opcodes.MemoryGrow:
-		f(offset+iter.Position(), "memidx", iter.Varint())
+		return f(offset+iter.Position(), "memidx", iter.Varint()), nil
+
 	case opcodes.MemoryCopy:
-		f(offset+iter.Position(), "dst memidx", iter.Varint())
-		f(offset+iter.Position(), "src memidx", iter.Varint())
+		return f(offset+iter.Position(), "dst memidx", iter.Varint()) +
+			f(offset+iter.Position(), "src memidx", iter.Varint()), nil
+
 	case opcodes.I32Sub, opcodes.I32Add, opcodes.I32Or, opcodes.I32Xor,
 		opcodes.I32GtS, opcodes.I32Eqz, opcodes.I32And, opcodes.I32Ne,
 		opcodes.I32Eq, opcodes.I32GtU, opcodes.I32LeU, opcodes.I32Shl,
@@ -93,6 +113,7 @@ func k(iter *binary.Iterator, offset int, opcode opcodes.Opcode) {
 		opcodes.I32TruncSatF32S, opcodes.I32Rotr, opcodes.I32Rotl:
 		// no immediate arguments
 		return
+
 	case opcodes.I64ExtendI32S, opcodes.I64Eqz, opcodes.I64GtU,
 		opcodes.I64ShrU, opcodes.I64GtS, opcodes.I64Sub, opcodes.I64Add,
 		opcodes.I64And, opcodes.I64Ne, opcodes.I64Eq, opcodes.I64LeU,
@@ -105,6 +126,7 @@ func k(iter *binary.Iterator, offset int, opcode opcodes.Opcode) {
 		opcodes.I64Extend16S, opcodes.I64Extend32S:
 		// no immediate arguments
 		return
+
 	case opcodes.F64Mul, opcodes.F64Add, opcodes.F64Sub,
 		opcodes.F64Div, opcodes.F64Min, opcodes.F64Max,
 		opcodes.F64Copysign, opcodes.F64Sqrt, opcodes.F64Ceil,
@@ -117,6 +139,7 @@ func k(iter *binary.Iterator, offset int, opcode opcodes.Opcode) {
 		opcodes.F64Ne:
 		// no immediate arguments
 		return
+
 	case opcodes.F32ConvertI32S, opcodes.F32ConvertI32U, opcodes.F32ConvertI64S,
 		opcodes.F32ConvertI64U, opcodes.F32DemoteF64, opcodes.F32Mul,
 		opcodes.F32Add, opcodes.F32Sub, opcodes.F32Div, opcodes.F32Min,
@@ -127,12 +150,14 @@ func k(iter *binary.Iterator, offset int, opcode opcodes.Opcode) {
 		opcodes.F32Ge, opcodes.F32Eq, opcodes.F32Ne:
 		// no immediate arguments
 		return
+
 	case opcodes.End, opcodes.Drop, opcodes.Select,
 		opcodes.Unreachable, opcodes.Return:
 		// no immediate arguments
 		return
+
 	default:
-		panic(fmt.Sprintf("unknown opcode: %02x\n", opcode))
+		return "", fmt.Errorf("unknown opcode: %02x\n", opcode)
 	}
 }
 
@@ -153,9 +178,9 @@ func typeToString(b byte) string {
 	}
 }
 
-func f(offset int, label string, x any) {
+func f(offset int, label string, x any) string {
 	str := fmt.Sprintf("%s: %s", p(offset), valueToHex(x))
-	fmt.Printf("%s; %s\n", pad(str, 50), label)
+	return fmt.Sprintf("%s; %s\n", pad(str, 50), label)
 }
 
 func pad(str string, n int) string {
