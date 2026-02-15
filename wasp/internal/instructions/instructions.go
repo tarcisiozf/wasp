be33aff891e032d67d86f3ffff6aa011575fe632
Tarcisio Ferraz <tarcisio.ferraz@smartcontract.com>
2026-02-15 20:07:08 -0300

    extensions ix list

--- wasp/internal/instructions/instructions.go
+++ wasp/internal/instructions/instructions.go
@@ -14,20 +14,33 @@ type instruction struct {
 }
 
 func (i instruction) String() any {
-	return fmt.Sprintf("%s(0x%x)", opcodes.Name(i.Opcode), i.Opcode)
+	return fmt.Sprintf("%s(0x%x)", i.Opcode.String(), i.Opcode)
 }
 
 var instructions = make([]instruction, 256)
+var extensions = make([]instruction, 20)
 
 func addInstruction(opcode opcodes.Opcode, handler handler) instruction {
-	if instructions[opcode].Opcode != 0 {
+	op := uint16(opcode)
+	if op > 0xFF {
+		return addInstructionToList(extensions, op&0xFF, opcode, handler)
+	}
+	return addInstructionToList(instructions, op, opcode, handler)
+}
+
+func addInstructionToList(list []instruction, index uint16, opcode opcodes.Opcode, handler handler) instruction {
+	if list[index].Opcode != 0 {
 		panic(fmt.Sprintf("instruction already defined: %v", opcode))
 	}
-	instructions[opcode].Opcode = opcode
-	instructions[opcode].Handler = handler
-	return instructions[opcode]
+	list[index].Opcode = opcode
+	list[index].Handler = handler
+	return list[index]
 }
 
 func Instruction(opcode opcodes.Opcode) instruction {
+	op := uint16(opcode)
+	if op > 0xFF {
+		return extensions[op&0xFF]
+	}
 	return instructions[opcode]
 }
